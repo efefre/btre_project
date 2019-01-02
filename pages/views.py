72af2210521b765ad61d0b155ee968b17dfe9856
Kasia <efefre@icloud.com>
2019-01-02 22:15:40 +0100

    Change index view. Add template: index.html

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'pages/index.html')
