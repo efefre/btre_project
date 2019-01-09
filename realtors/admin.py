444aca1287cf51dff045a6cfbe167264752de03b
Kasia <efefre@icloud.com>
2019-01-09 16:10:33 +0100

    Customize view for "Realtors" on admin webpage

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from realtors.models import Realtor
 
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
 # Register your models here.
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
