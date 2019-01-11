4750dd4f50c652bc8f64ed4f2218d14c2676ddb3
Kasia <efefre@icloud.com>
2019-01-11 21:17:47 +0100

    Add filter to Listing.objects
    Display only id is published.

--- listings/views.py
+++ listings/views.py
@@ -5,7 +5,7 @@ from .models import Listing
 
 # Create your views here.
 def index(request):
-    listings = Listing.objects.order_by('-list_date')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     paginator = Paginator(listings, 6)
     page = request.GET.get('page')
