587565d760bc6e8eca41f4cb6bb88a31788c0d64
Kasia <efefre@icloud.com>
2019-01-12 18:10:37 +0100

    Add new queryset_list with filter: keywords

--- listings/views.py
+++ listings/views.py
@@ -32,6 +32,12 @@ def listing(request, listing_id):
 def search(request):
     queryset_list = Listing.objects.order_by('-list_date')
 
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
     context = {
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
