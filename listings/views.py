92238c71b194dd44513478c287b67b028141da0f
Kasia <efefre@icloud.com>
2019-01-12 17:57:34 +0100

    Add queryset_list in search view (search results)

--- listings/views.py
+++ listings/views.py
@@ -30,10 +30,13 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
 
     context = {
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
-        'state_choices': state_choices
+        'state_choices': state_choices,
+        'listings': queryset_list
     }
+
     return render(request, 'listings/search.html', context)
