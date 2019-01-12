ef7da3b8b4afd5625f1ce6bb8af3e3f7cd209054
Kasia <efefre@icloud.com>
2019-01-12 12:34:22 +0100

    Check if listing exist. If not return 404 error

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
@@ -19,6 +19,8 @@ def index(request):
 
 
 def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
     return render(request, 'listings/listing.html')
 
 
