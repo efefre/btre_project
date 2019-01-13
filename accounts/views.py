a87669bcaeeda0935398bea73fe1b3a690c68b48
Kasia <efefre@icloud.com>
2019-01-13 17:04:29 +0100

    Add login view (accounts app)

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def register(request):
     return render(request, 'accounts/register.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
