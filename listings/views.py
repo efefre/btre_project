467f11d7c574bb49c2938141d342a01b9876cc2a
Kasia <efefre@icloud.com>
2019-01-13 15:30:44 +0100

    Add 'values': request.GET to search context

--- listings/views.py
+++ listings/views.py
@@ -66,7 +66,8 @@ def search(request):
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
         'state_choices': state_choices,
-        'listings': queryset_list
+        'listings': queryset_list,
+        'values': request.GET
     }
 
     return render(request, 'listings/search.html', context)
