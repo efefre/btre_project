7257dfcbc69a6710bf62ae706909de8de9a71497
Kasia <efefre@icloud.com>
2019-01-16 21:40:33 +0100

    Modify register view for testing alert (error message)

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         # Register User
-        pass
+        messages.error(request, 'Testing error message')
+        return redirect(register)
     else:
         return render(request, 'accounts/register.html')
 
