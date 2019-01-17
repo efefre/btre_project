4e67199d2d0b256f239bb07c8d7311c9feb9db62
Kasia <efefre@icloud.com>
2019-01-17 21:48:33 +0100

    Check if user exist. If yes - log in (login form)

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -45,6 +45,14 @@ def login(request):
         # Get form values
         username = request.POST['username']
         password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+
     else:
         return render(request, 'accounts/login.html')
 
