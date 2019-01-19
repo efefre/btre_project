4b1fd3c4a09aa0f3aa6bae0d5c5b2d80ef3a270f
Kasia <efefre@icloud.com>
2019-01-19 18:49:52 +0100

    Add email confi in settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -142,3 +142,10 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
